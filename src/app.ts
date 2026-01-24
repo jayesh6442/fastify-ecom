1bd98d4a898ce4503a8d7d058a17abaa53e93301
jayesh <jayeshsarvaiya9913@gmail.com>
2026-01-24 15:59:35 +0530

    user end point audit

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@
 import Fastify from 'fastify';
 import { dbPlugin } from './plugins/db.js';
 import { orderRoutes, productRoutes } from './modules/products/route.js';
+import { userRoutes } from './modules/users/routes.js';
 
 export function buildApp() {
     const app = Fastify({ logger: true });
@@ -26,6 +27,7 @@ export function buildApp() {
     app.register(dbPlugin);
     app.register(productRoutes);
     app.register(orderRoutes);
+    app.register(userRoutes)
     app.get('/health', async (req, reply) => {
         const result = await app.db.query('SELECT 1');
         return { status: 'ok', db: result.rowCount === 1 };
